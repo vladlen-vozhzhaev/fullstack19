f534acf7d3f4461414d21764b29a2de1dadf65af
Vladlen <vladlen@vozhzhaev.ru>
2022-05-18 21:35:15 +0300

    Use handlebars

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const session = require('express-session')
+const HmacSHA256 = require('crypto-js/hmac-sha256');
 const mysql = require("mysql2");
+const multer = require("multer");
+const cookieParser = require('cookie-parser');
+const {engine} = require('express-handlebars');
 const app = express()
 const port = 3000
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = express.urlencoded({extended: false});
+app.use(cookieParser('secret key'))
+app.use(express.static(`${__dirname}/public`));
+app.engine('handlebars', engine());
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     database: "fullstack19",
     password: ""
 });
+
+
+
 connection.connect(function (err) {
     if (err) {
         return console.error(err.message);
@@ -18,7 +30,16 @@ connection.connect(function (err) {
 });
 
 app.get('/', (req, res) => {
-    res.send('<a href="/reg">Регистрация</a>|<a href="/login">Вход</a>');
+    res.render('home', { title: 'Greetings form Handlebars' })
+});
+app.get('/cabinet', (req, res) => {
+    console.log('Cookie: ', req.cookies['token']);
+    if(req.cookies['token'] === '12345ABCDE'){
+        res.send('Личный кабинет');
+    }else{
+        res.send('Доступ запрещен');
+    }
+
 });
 app.get('/reg', (req, res)=>{
     res.sendFile(__dirname + "/reg.html")
@@ -26,25 +47,52 @@ app.get('/reg', (req, res)=>{
 app.get('/login', (req, res)=>{
     res.sendFile(__dirname + "/auth.html")
 })
-app.post('/reg',  urlencodedParser, (req, res)=>{
-    console.log(req.body);
-    connection.query("SELECT id FROM users WHERE email=?", [req.body.email], function (err, res){
-        if(res.length){console.log("exist")}
-        else{
-            const user = [req.body.name, req.body.lastname, req.body.email, req.body.pass];
+app.post('/reg', multer().fields([]), (req, res)=>{
+    let sendResult  = "success";
+    let email = req.body.email.toLowerCase();
+    connection.query("SELECT id FROM users WHERE email=?", [email], function (err, res1){
+        console.log(res1.length);
+        if(res1.length){
+            console.log("exist");
+            sendResult = "exist";
+            res.send(sendResult);
+        }else{
+            let pass = (HmacSHA256(req.body.pass, 'secret').toString());
+            const user = [req.body.name, req.body.lastname, email, pass];
             connection.query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES (?,?,?,?)", user,
                 function (error, result, metadata) {
                     console.log(error);
                     console.log(result);
+                    res.send(sendResult);
                 })
         }
-    })
-    res.send("success");
+    });
+
 })
-app.post('/login', urlencodedParser, (req,res )=>{
-    console.log(req.body);
-    res.send("success");
+app.post('/login', multer().fields([]), (req,res )=>{
+    let email = req.body.email.toLowerCase();
+    connection.query("SELECT * FROM users WHERE email=?", [email], function (err, res1){
+        console.log(res1);
+        if(res1.length){
+            let pass = (HmacSHA256(req.body.pass, 'secret').toString());
+            if(pass === res1[0].pass){
+                res.cookie('token', '12345ABCDE');
+                res.send("success");
+            }else {
+                res.send("error");
+            }
+        }
+        else{
+            res.send("error");
+        }
+    });
 });
+
+app.get('/logout', (req, res)=>{
+    res.clearCookie('token');
+    res.send('success');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
